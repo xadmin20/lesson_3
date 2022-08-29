ecd66c285b693596a3dd17c17104887a01708d9f
Nick <74838258+xadmin20@users.noreply.github.com>
2022-08-29 13:34:43 +0300

    Задача 21

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-//14212 -> нет
-//12821 -> да
-//23432 -> да
-string isPalindrom(int s)
-{   
-    string newString = s.ToString();
-    for (int i = 0; i < newString.Length / 2; ++i)
-        if (newString[i] != newString[newString.Length - 1 - i]) return "Нет";
-    return "Да";
-}
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+try{
+    Console.Write("Введите координаты x1: "); double x1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите координаты y1: "); double y1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите координаты z1: "); double z1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write(isPalindrom(14212));
-Console.WriteLine();
-Console.Write(isPalindrom(12821));
-Console.WriteLine();
-Console.Write(isPalindrom(23432));
+    Console.Write("Введите координаты x2: "); double x2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите координаты y2: "); double y2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите координаты z2: "); double z2 = Convert.ToDouble(Console.ReadLine());
+
+    double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    Console.WriteLine("{0:0.00}", dist);
+}
+catch { Console.Write("Введите число!"); }
